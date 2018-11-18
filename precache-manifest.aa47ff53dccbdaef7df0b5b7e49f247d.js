8ed6b23b733a990e8fae5c0157488efe789de4ff
Le Dai Vuong <v000770@pc-000770-3.local>
2018-11-18 17:53:41 +0700

    Updates

---
+++ precache-manifest.aa47ff53dccbdaef7df0b5b7e49f247d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae265bfad00735e37c33",
+    "url": "/random-quote/static/js/runtime~main.ae265bfa.js"
+  },
+  {
+    "revision": "c6d1e8ab9836997c9be2",
+    "url": "/random-quote/static/js/main.c6d1e8ab.chunk.js"
+  },
+  {
+    "revision": "5a76055550049ad5d2b2",
+    "url": "/random-quote/static/js/1.5a760555.chunk.js"
+  },
+  {
+    "revision": "c6d1e8ab9836997c9be2",
+    "url": "/random-quote/static/css/main.27d338bf.chunk.css"
+  },
+  {
+    "revision": "103e394d2ee0cb4400595ae5ac826ad3",
+    "url": "/random-quote/index.html"
+  }
+];
